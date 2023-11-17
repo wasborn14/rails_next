9f1b714d73a724efe8f8a9f9416db57ed07c19fc
wasborn14 <archibald.ff14@gmail.com>
2023-11-18 07:39:18 +0900

    chapter 21 end

---
+++ rails/app/models/user.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+  include DeviseTokenAuth::Concerns::User
+  has_many :articles, dependent: :destroy
+end
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   namespace :api do
     namespace :v1 do
       get "health_check", to: "health_check#index"
+      mount_devise_token_auth_for "User", at: "auth"
+      namespace :current do
+        resource :user, only: [:show]
+        resources :articles, only: [:index, :show, :create, :update]
+      end
+      resources :articles, only: [:index, :show]
     end
   end
 end
